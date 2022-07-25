a12458431ddc5b4f9a97178e39c506b5f6dc1972
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-07-26 00:44:36 +0300

    password fix

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -23,11 +23,10 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         };
     }
 
-
     /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
     protected String[] getServletMappings() {
-        return new String[]{""};
+        return new String[]{"/"};
     }
 
     @Override
